786b597ac7f61e05772a2b2cfeab98987b5c648e
sanix-darker <s4nixd@gmail.com>
2023-09-23 23:29:24 +0200

    feat: add resty as deps + start writing the chatGPT client

--- apis/chatGpt.go
+++ apis/chatGpt.go
@@ -1,11 +1,54 @@
 package apis
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/go-resty/resty/v2"
+)
+
 // RequestBuilder
 func RequestBuilder() {
 
 }
 
 // Handler
-func Handler() {
+func ChatGptHandler() {
+
+	const (
+		apiEndpoint = "https://api.openai.com/v1/chat/completions"
+	)
+
+	// Use your API KEY here
+	apiKey := "YOUR API KEY HERE"
+	client := resty.New()
+
+	response, err := client.R().
+		SetAuthToken(apiKey).
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"model":      "gpt-3.5-turbo",
+			"messages":   []interface{}{map[string]interface{}{"role": "system", "content": "Hi can you tell me what is the factorial of 10?"}},
+			"max_tokens": 50,
+		}).
+		Post(apiEndpoint)
+
+	if err != nil {
+		log.Fatalf("Error while sending send the request: %v", err)
+	}
+
+	body := response.Body()
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error while decoding JSON response:", err)
+		return
+	}
+
+	// Extract the content from the JSON response
+	content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+	fmt.Println(content)
 
 }
--- go.mod
+++ go.mod
@@ -11,12 +11,14 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-resty/resty/v2 v2.8.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/muesli/mango v0.1.0 // indirect
 	github.com/muesli/mango-pflag v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.15.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
